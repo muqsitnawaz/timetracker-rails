57fdec46de367e3a02a91ffddb0837ca58a24869
muqsitnawaz <muqsitnawaz@gmail.com>
2015-07-26 16:29:56 -0400

    Started working on View

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,8 +1,10 @@
 class CompaniesController < ApplicationController
+	# method for defining the index of the company
 	def index
 		@companies = Company.all;
 	end
 
+	# method for showing an individual company
 	def show
 		@company = Company.find(params[:id]);
 	end
